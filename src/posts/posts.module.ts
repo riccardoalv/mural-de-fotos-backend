e474b0ce7d919b894dc5f6b2a57cc0301aeb5ba2
Ricardo Alves da Silva <ricardcpu@gmail.com>
2025-03-30 13:52:15 -0400

    fix

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,11 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaService } from 'src/databases/prisma/prisma.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EmailListener } from 'src/notification/email.listener';
 
 @Module({
   imports: [EventEmitterModule.forRoot()],
   controllers: [PostsController],
-  providers: [PostsService, PrismaService],
+  providers: [PostsService, PrismaService, EmailListener],
 })
 export class PostsModule { }
