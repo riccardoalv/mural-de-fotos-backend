816a7e0345335c674c94f0d8155680ff3e23da6a
Ricardo Alves da Silva <ricardcpu@gmail.com>
2025-04-26 21:38:26 -0400

    add email notification

--- src/notification/email.listener.ts
+++ src/notification/email.listener.ts
@@ -1,19 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { EmailService } from './email.service';
+import { Comment, Post } from '@prisma/client';
+import { PrismaService } from 'src/databases/prisma/prisma.service';
 
 @Injectable()
 export class EmailListener {
-  constructor(private readonly emailService: EmailService) { }
+  constructor(
+    private readonly emailService: EmailService,
+    private readonly prisma: PrismaService,
+  ) { }
 
   @OnEvent('comment.created')
   async handleCommentCreatedEvent(payload: {
-    comment: any;
+    comment: Comment;
     postId: string;
     userId: string;
   }) {
     const { comment } = payload;
 
-    await this.emailService.sendCommentNotification(comment);
+    const post = await this.prisma.post.findUnique({
+      where: {
+        id: payload.postId,
+      },
+      include: {
+        comments: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+
+    const comments = post!.comments;
+
+    const users = comments.map((c) => c.user);
+
+    await this.emailService.sendCommentNotification(
+      comment,
+      post as Post,
+      users,
+    );
   }
 }
--- src/notification/email.service.ts
+++ src/notification/email.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as ejs from 'ejs';
 import { Resend } from 'resend';
 import { emailTemplate } from './templates/comment-notification-email.template';
+import { Comment, Post, User } from '@prisma/client';
 
 @Injectable()
 export class EmailService {
@@ -11,13 +12,15 @@ export class EmailService {
     this.resend = new Resend(process.env.RESEND_API_KEY);
   }
 
-  async sendCommentNotification(comment: any) {
+  async sendCommentNotification(comment: Comment, post: Post, users: User[]) {
     const html = await ejs.render(emailTemplate, { comment });
+
+    const to = users.map((user) => user.email).filter(Boolean);
 
     return this.resend.emails.send({
       from: 'Mural de Fotos <noreply@mural.earthdoor.cc>',
-      to: comment.post.user.email,
-      subject: 'Você tem um novo comentário no seu post!',
+      to,
+      subject: `Um novo comentário foi adicionado no post: "${post.caption}"!`,
       html,
     });
   }
